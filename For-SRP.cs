1e6dbd329fbe2af8b4ca1ccba59b144912c4b669
adavletyarov <adavletyarov@fors.ru>
2020-10-22 18:19:39 +0300

    добавлен класс FileStorage

--- For-SRP.cs
+++ For-SRP.cs
@@ -1,32 +1,92 @@
-    class Player
+public class Player
+{
+    private float _armor;
+
+    public event EventHandler<PlayerInfo> OnLoad;
+    public event Action OnSave;
+
+    public Player(float health, float armor, int id)
     {
-        private float _health;
-        private float _armor;
-        private int _id;
+        Health = health;
+        _armor = armor;
+        Id = id;
 
-        public float Health => _health;
+        Load();
+    }
 
-        public Player(float health, float armor, int id)
+    public int Id { get; }
+
+    public float Health { get; private set; }
+
+    private void Load()
+    {
+        var eventArgs = new PlayerInfo();
+
+        OnLoad?.Invoke(this, eventArgs);
+        Health = eventArgs.Health;
+    }
+    
+    public void ApplyDamage(float damage)
+    {
+        var healthDelta = damage - _armor;
+        Health -= healthDelta;
+        _armor /= 2;
+
+        Console.WriteLine($"Вы получили урона - {healthDelta}");
+        OnSave?.Invoke();
+    }
+}
+
+public class PlayerInfo : EventArgs
+{
+    public float Health { get; set; }
+}
+
+public class FileStorage : IDisposable
+{
+    private readonly Player _player;
+    private static readonly List<int> Players = new List<int>();
+
+    public FileStorage(Player player)
+    {
+        _player = player;
+
+        try
         {
-            _health = health;
-            _armor = armor;
-            _id = id;
+            if (Players.Contains(player.Id))
+                throw new InvalidOperationException($"Для игрока с ID={_player.Id} уже создано файловое хранилище!");
 
-            if (File.Exists(($"user_{_id}.data")))
-            {
-                var data = File.ReadAllText($"user_{_id}.data");
-                _health = float.Parse(data);
-            }
+            Players.Add(_player.Id);
+            _player.OnLoad += OnLoad;
+            _player.OnSave += OnSave;
         }
-
-        public void ApplyDamage(float damage)
+        catch (Exception e)
         {
-            float healthDelta = damage - _armor;
-            _health -= healthDelta;
-            _armor /= 2;
-
-            Console.WriteLine($"Вы получили урона - {healthDelta}");
-
-            File.WriteAllText($"user_{_id}.data", _health.ToString());
+            Console.WriteLine(e.Message);
         }
     }
+
+    public void Dispose()
+    {
+        _player.OnLoad -= OnLoad;
+        _player.OnSave -= OnSave;
+        Players.Remove(_player.Id);
+    }
+
+    private void OnSave()
+    {
+        File.WriteAllText($"user_{_player.Id}.data", _player.Health.ToString(CultureInfo.InvariantCulture));
+    }
+
+    private void OnLoad(object sender, PlayerInfo playerInfo)
+    {
+        var data = string.Empty;
+
+        if (File.Exists(($"user_{_player.Id}.data")))
+        {
+            data = File.ReadAllText($"user_{_player.Id}.data");
+        }
+
+        if (float.TryParse(data, out var parseResult)) playerInfo.Health = parseResult;
+    }
+}
